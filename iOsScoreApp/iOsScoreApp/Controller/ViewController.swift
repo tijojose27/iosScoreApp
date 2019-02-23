d404c22956db71cd4de21aeeebb249f2c8f6f9fe
TJ <tijojose27@yahoo.com>
2019-02-23 13:05:34 -0600

    the app is complete

---
+++ iOsScoreApp/iOsScoreApp/Controller/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  iOsScoreApp
+//
+//  Created by Tijo Jose on 2/22/19.
+//  Copyright © 2019 Tijo Jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var team1ScoreLabel: UILabel!
+    @IBOutlet weak var team2ScoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func team1ScoreBtn(_ sender: Any) {
+    }
+    
+    @IBAction func team2ScoreBtn(_ sender: Any) {
+    }
+    
+    @IBAction func team1FoulBtn(_ sender: Any) {
+    }
+    
+    @IBAction func team2FoulBtn(_ sender: Any) {
+    }
+    
+    @IBAction func resetBtn(_ sender: Any) {
+    }
+    
+    
+}
+
---
+++ iOsScoreApp/iOsScoreApp/Model/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  iOsScoreApp
+//
+//  Created by Tijo Jose on 2/23/19.
+//  Copyright © 2019 Tijo Jose. All rights reserved.
+//
+
+import Foundation
